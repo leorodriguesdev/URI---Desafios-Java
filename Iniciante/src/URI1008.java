8720ee3b7eac9128f8f6ba84447c32a4bd6fe7c0
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2021-04-07 00:28:04 -0300

    Up new challenge
    add new challenge

---
+++ Iniciante/src/URI1008.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class URI1008 {
+
+	public static void main(String[] args) {
+		
+        Scanner leitor = new Scanner(System.in);
+        int NUMBER = leitor.nextInt();
+        int HOURS = leitor.nextInt();
+        double VALUE = leitor.nextDouble();
+
+        double SALARY = VALUE * HOURS;
+                
+        System.out.println("NUMBER = "+ NUMBER);
+        System.out.printf("SALARY = U$ %.2f", SALARY);
+        System.out.println();
+
+	}
+
+}
